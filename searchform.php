58a0f61e3986bb82b84f5c0f1ffd56f717b7e1a4
slastenovd <slastenovd@mail.ru>
2016-08-08 16:35:14 +1000

    auto commit

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
   <div class="">
    <div class="form-group">
 
-    <form role="search" method="get" id="searchform" action="<?php bloginfo("template_url") ?>">
+    <form role="search" method="get" id="searchform" action="<?php  bloginfo("url") ?>">
       <div class="input-group">
         <input type="search" placeholder="Поиск.." class="form-control" id="s" name="s">
         <span class="input-group-btn">
