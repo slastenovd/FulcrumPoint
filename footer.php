93c558c8a3b32aa33af7362f490c517f94fd780f
slastenovd <slastenovd@mail.ru>
2016-09-22 13:44:02 +1000

    3

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
             <div class="col-md-3 contact">
             <h4>Контакты</h4>
             <ul class="list-inline">
-                <li><span><i class="fa fa-map-marker"></i></span><a href="contacts#about-us"> Комсомольск-на-Амуре, <?php echo get_option('theme_contacttext'); ?></a></li>
+                <li><span><i class="fa fa-map-marker"></i></span><a href="/contacts#about-us"> Комсомольск-на-Амуре, <?php echo get_option('theme_contacttext'); ?></a></li>
             <li><span><i class="fa fa-phone"></i></span> <?php echo get_option('site_telephone'); ?> </li>
             <li><span><i class="fa fa-envelope"></i></span> <?php echo get_option('site_email'); ?> </li>
             </ul>
